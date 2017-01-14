2b8864033499534ac6d9ddd85111cc90deec37ef
Shisus787 <jessdevil97@yahoo.com.ar>
2017-01-14 15:43:41 -0300

    Update Pyglatin translator.py

--- Pyglatin translator.py
+++ Pyglatin translator.py
@@ -1,15 +1,22 @@
-//Ultima palabra a agregar
+#Last word to add
 pyg = 'ay'
-//Hace la pregunta
+
+#Asks the question
 original = raw_input('Enter a word:')
-//Para que la primera letra sea minuscula
+
+#Makes the first letter lowercase
 word = original.lower()
+
+#It takes the first letter of word
 first = word[0]
-//Para separar las letras 
+
+#It slices the letters from 1 to x 
 new_word = new_word[1:]
-//Combinar strings
+
+#Concatenation of strings
 new_word = word + first + pyg
-//Condicion para que la palabra se escriba
+
+#Condition to print the word, it must be a word, no numbers and more than 0 characters
 if len(original) > 0 and original.isalpha():
     print new_word
 else:
